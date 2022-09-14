0824599ed28ac7e7feeb566665fbbf046caed1a5
Link <stereo_house01@mail.ru>
2022-09-14 15:43:32 +0300

    Example003_sum  работа с суммо числе

---
+++ Example/Exampel003_sum/Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int numberA = 45;
+        int numberb = 9;
+        int result = (numberA + numberb);
+Console.WriteLine(result);
+    }
+}       
