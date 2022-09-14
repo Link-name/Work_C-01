baa5606a11d7470630968762b272bdc80aa79fb1
Link <stereo_house01@mail.ru>
2022-09-14 16:29:01 +0300

    выполнение сложения с рандобными числами + показ какие числа складываются

--- Example/Exampel003_sum/Program.cs
+++ Example/Exampel003_sum/Program.cs
@@ -1,10 +1,20 @@
-﻿internal class Program
+﻿using System.Diagnostics;
+
+[DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
+internal class Program
 {
     private static void Main(string[] args)
     {
-        int numberA = 45;
-        int numberb = 9;
+        int numberA = new Random().Next(1, 10); //1.2.3.4...9
+        Console.WriteLine(numberA);
+        int numberb = new Random().Next(1, 10);
+        Console.WriteLine(numberb);
         int result = (numberA + numberb);
 Console.WriteLine(result);
+    }
+
+    private string GetDebuggerDisplay()
+    {
+        return ToString();
     }
 }       
